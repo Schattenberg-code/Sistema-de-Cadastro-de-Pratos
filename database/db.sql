37933671a91bed2d7c19f942b7d6cf3728ebb429
Schattenberg-code <lucas_schattenberg@estudante.sesisenai.org.br>
2026-08-17 08:24:09 -0300

    feat - adiciona arquivos de CRUD dos pratos e usuario, tambem organiza pastas e arquivos.

--- database/db.sql
+++ database/db.sql
@@ -1,14 +1,18 @@
-CREATE database if not exists sistema_cadastrar_pratos_fixluc;
+CREATE DATABASE IF NOT EXISTS sistema_cadastrar_pratos_fixluc;
 USE sistema_cadastrar_pratos_fixluc;
 
-CREATE TABLE usuarios (
+CREATE TABLE if NOT EXISTS usuarios (
     id INT AUTO_INCREMENT PRIMARY KEY,
     nome VARCHAR(30),
     gmail VARCHAR(255),
     senha VARCHAR(20)
 );
 
-CREATE TABLE pratos (
+CREATE TABLE if NOT EXISTS pratos (
     id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(40),
+    preco int,
+    descricao VARCHAR(255),
+    
     id_usuario INT FOREIGN KEY
 );
