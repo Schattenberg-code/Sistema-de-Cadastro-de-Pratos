fa90c201875d52597fcfcd38d795703882866f57
Schattenberg-code <lucas_schattenberg@estudante.sesisenai.org.br>
2026-08-17 08:29:13 -0300

    feat - adiciona html ao index.php

--- index.php
+++ index.php
@@ -2,3 +2,19 @@
 session_start(); 
 
 include("infra/conexao.php");
+
+
+
+?>
+
+
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
